973a45662d2b7cc1722a6eda01db7809f6184f13
lucas <lucaslmota0431@gmail.com>
2021-10-10 20:29:57 -0300

    feita a listagens de informacoes

--- front/src/components/list-employee/ListEmployeeComponent.vue
+++ front/src/components/list-employee/ListEmployeeComponent.vue
@@ -1,15 +1,65 @@
 <template>
-  <div class="row justify-content-center">
-      <h1>Listar funcionário</h1>
+  <div>
+    <h3 class="text-center">Listar funcionário</h3>
+    <table class="table table-striped mt-3">
+      <thead>
+        <tr>
+          <th class="text-center">Nome do funcionário</th>
+          <th class="text-center">Cargo</th>
+          <th class="text-center">Salário</th>
+          <th class="text-center">Nascimento</th>
+          <th class="text-center">Registro do funcionário</th>
+          <th class="text-center">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.employee_id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.job_role }}</td>
+          <td>{{ employee.salary }}</td>
+          <td>{{ employee.birth }}</td>
+          <td>{{ employee.employee_registration }}</td>
+          <td>
+            <router-link
+              :to="{ name: 'update', params: { id: employee.employee_id } }"
+              class="btn btn-success"
+            >
+              <i class="fas fa-pen-square"></i> Editar</router-link
+            >
+          </td>
+          <td>
+            <button class="btn btn-danger">
+              <i class="fas fa-trash-alt"></i> Deletar
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import EmployeeService from "../../services/EmployeeService";
 export default {
+  name: "ListEmployeeComponent",
+  data() {
+    return {
+      employees: [],
+    };
+  },
 
-}
+  mounted() {
+    this.listAllEmployees();
+  },
+
+  methods: {
+    async listAllEmployees() {
+      const response = await EmployeeService.getEmployees();
+      this.employees = response;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/nprogress/nprogress.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false
 
